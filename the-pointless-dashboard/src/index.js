3f95d90f5ed9b163b1227a1da79edf77bbb9f487
ninajhun <ninajhun@berkeley.edu>
2021-04-18 13:13:50 -0700

    remove unneccesary files & add project file stucture

--- the-pointless-dashboard/src/index.js
+++ the-pointless-dashboard/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 const { REACT_APP_STEPZEN_API_KEY, REACT_APP_STEPZEN_URI } = process.env;
@@ -23,8 +23,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
