2d77e69d6db1247eeefe922c9ed0fc33fddb6a9c
ninajhun <ninajhun@berkeley.edu>
2021-04-19 13:02:22 -0700

    WIP: set up dashboard; Add google fonts, bootstrap 4

---
+++ the-pointless-dashboard/src/components/dashboard/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">Navbar</span>
+        </div>
+      </nav>
+
+    </>
+  )
+}
---
+++ the-pointless-dashboard/src/views/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/dashboard/header'
+
+export default function Dashboard() {
+  return (
+    <>
+      <Header />
+      <h1>Dashboard</h1>
+      <p>hello</p>
+    </>
+  )
+}
