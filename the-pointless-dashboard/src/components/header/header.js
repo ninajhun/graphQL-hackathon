fcfb1b123fcff8f8fe68bc896c956d8f94b35137
Chris Gitmed <cgitmed@gmail.com>
2021-04-22 00:09:46 -0700

    Fixed bug in header; class should be className.

--- the-pointless-dashboard/src/components/header/header.js
+++ the-pointless-dashboard/src/components/header/header.js
@@ -4,9 +4,9 @@ import "./header.css"
 export default function Header() {
   return (
     <>
-      <nav class="navbar navbar-light bg-light">
-        <div class="container-fluid">
-          <span class="navbar-brand mb-0 h1 ">
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1 ">
             <span className="emoji mx-3" >&#128021;</span>
             The Pointless Dashboard</span>
         </div>
